c72983e3bcb1d8242acf845ca6cdcfb57a181321
seongyunlee <private.seongyun@gmail.com>
2024-01-18 22:28:24 +0900

    Refactor graph traversal and calculate shortest path

---
+++ b15989.py
@@ -0,0 +1,6 @@
+import sys
+input = sys.stdin.readline
+ans = []
+for _ in range(int(input())):
+    A = int(input())
+    ans.append(N+N//2+N//3)
--- b1761.py
+++ b1761.py
@@ -1,26 +1,43 @@
-A = {}
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(50000)
 N = int(input())
 E = {i:[] for i in range(1,N+1)}
-for _ in range(N):
+for _ in range(N-1):
     f,t,c = map(int,input().split())
     E[f].append([t,c])
     E[t].append([f,c])
-P = {i:[] for i in range(1,N+1)}
-AC = [0]*(N+1)
+P = [{} for i in range(1,N+1)]
 L = [0]*(N+1)
 P = [None]*(N+1)
-def dfs(prev,idx,cost,level):
+P[1] = [-1,0]
+def dfs(prev,idx,level):
     L[idx]=level
-    AC[idx] = cost
+    P[idx][0] = prev
+    up = 1
+    while True:
+        if P[P[idx][i-1]]
+        P[idx][up]
     for ch,co in E[idx]:
+        if prev == ch : continue
         P[ch] = [idx,co]
-        if ch==prev:continue
-        dfs(idx,ch,cost+co,level+1)
-dfs(-1,1,0,0)
-for _ in range(int(input(()))):
+        dfs(idx,ch,level+1)
+dfs(-1,1,0)
+A = []
+for _ in range(int(input())):
     a,b = map(int,input().split())
     ans = 0
-    while L[a]==L[b]:
+    while L[a]!=L[b]:
         if L[a]<L[b]:
-            cost += P[b][1]
+            ans += P[b][1]
             b = P[b][0]
+        else:
+            ans += P[a][1]
+            a = P[a][0]
+    while a!=b:
+        ans += P[b][1]
+        ans += P[a][1]
+        a = P[a][0]
+        b = P[b][0]
+    A.append(str(ans))
+print('\n'.join(A))
